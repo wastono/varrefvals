8f9cd02c7cedf487e9400b06d3bf39c305327866
wastono <was.tono@gmail.com>
2019-07-15 15:43:17 +0700

    update tag line and copyright year.

--- varrefvals.php
+++ varrefvals.php
@@ -1,24 +1,30 @@
 <?php
 
+//	varrefvals.php
+
 /*
-
-    Varrefvals. Simple code compiler of no $ sign for PHP.
-    Copyright (C) 2016 Wastono
-
+	
+    Varrefvals. Simple code compiler for PHP code written without $ => :: -> ; signs.
+    Copyright (C) 2016 - 2019 Wastono
+	
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
     by the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-
+	
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
-
+	
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	
 */
+
+//	Varrefvals version 2.0
+
+
 
 ob_implicit_flush(true);
 echo "\n	Varrefvals (c) 2016 Wastono.\n";
